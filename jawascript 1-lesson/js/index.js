405ee09eb23458f02f1ac0b86a298807baa11829
depin1 <122809513+depin1@users.noreply.github.com>
2023-03-14 01:56:24 +0500

    Add files via upload

---
+++ jawascript 1-lesson/js/index.js
@@ -0,0 +1,481 @@
+// const number = +prompt("sizning pulingiz qancha");
+// const yourmoney = 500;
+// const hotel = 250;
+// const another = 120;
+
+// const all = ((yourmoney * 11000) + (hotel * 11000) + (another * 12000));
+
+// if (number >= all) {
+//     console.log("Oq y'ol");
+// } else {
+//     console.log(" Sizda yetarlicha mablash mavjud emas");
+// }
+
+
+
+
+
+
+// const year = +prompt("Nechinchi yilda tugulgansiz")
+
+// if (year >= 2001) {
+//     console.log("siz 21 asrda tugulga ekansiz")
+// } else {
+//     console.log("siz 20 asrda tugulgan ekansiz")
+// }
+
+
+
+
+
+
+// let x = +prompt("birinchi soni kiriting")
+// let y = +prompt("ikinchi soni kiriting")
+// let c = +prompt("ikkala sondi yigindisini kiriting")
+// if (x + y == c) {
+//     console.log(
+//         "tabriklaymiz siz spark avtomobil yutib oldingiz")
+// } else {
+//     console.log("siz bankdan 10 000$ qariz boldingiz")
+// }
+
+
+
+
+
+
+// const lesson = prompt("Oz'ingizga maqul dars jadvalini yozing");
+// if (lesson == "kechqurun") {
+//     console.log("Sizni oquv voqtingiz 18:30 da boshlanada")
+// } else if (lesson == "bilmasam") {
+//     console.log("kechqurun boladimi")
+// } else {
+//     console.log("Afsus bizda faqatgina kechqurungi voqtga darslar bor")
+// }
+
+
+
+// const firght = (true);
+// const two = (true);
+// if (firght || two) { console.log("mashina hayda olasiz") } else {
+//     console.log("siz moshina hayda olmaysiz")
+// }
+
+
+
+
+
+
+// const num2 = +prompt("2) Iltimos raqam kiriting");
+// if (num2 > 0) {
+//     alert(num2 + 1);
+// } else {
+//     alert(num2 + 2);
+// }
+
+
+
+// const num3 = +prompt("3) Iltimos raqam kiriting");
+// if (num3 > 0) {
+//     alert(`sizning javobingiz ${num3 + 1}`);
+// } else if (num3 == 0) {
+//     alert(100);
+// } else {
+//     alert(`! javobi ${num3 + 2}`);
+// }
+
+
+
+
+// const nu1 = +prompt("iltimos son kiriting");
+// const nu2 = +prompt("iltimos ikinchi soni son kiriting");
+// const nu3 = +prompt("iltimos uchinchi soni son kiriting");
+// if (nu1 > 0) {
+//     alert(nu1);
+// } else if (0 == nu1) {
+//     alert(100);
+// } else if (0 > nu2) {
+//     alert(`iltimos faqat musbat son ${nu1} kiriting`);
+// } else {
+//     alert(`iltimos faqat musbat son ${nu1} kiriting`);
+// }
+// if (nu2 > 0) {
+//     alert(nu1);
+// } else if (0 == nu2) {
+//     alert(100);
+// } else if (0 > nu2) {
+//     alert(`iltimos faqat musbat son ${nu2} kiriting`);
+// } else {
+//     alert(`iltimos faqat musbat son ${nu2} kiriting`);
+// }
+// if (nu3 > 0) {
+//     alert(nu3);
+// } else if (0 == nu3) {
+//     alert(100);
+// } else if (0 > nu3) {
+//     alert(`iltimos faqat musbat son ${nu3} kiriting`);
+// } else {
+//     alert(`iltimos faqat musbat son ${nu3} kiriting`);
+// }
+
+
+
+
+
+
+
+
+
+// const num10 = +prompt('6)iltimos birinchi son kiriting')
+// const num11 = +prompt('iltimos ikkinchi sonni ham kiriting')
+
+// if (num10 > num11) {
+//     console.log(`birinchi son katta ekan ${num10} > ${num11} dan`)
+// } else if (num10 == num11) {
+//     console.log(`ikkala son ham teng ekan ${num10 } va ${num11}
+//        `)
+// } else {
+//     console.log(`ikkinchi son katta ekan ${num11} > ${num10}
+//         dan`)
+// }
+
+
+
+
+
+
+// const num10 = +prompt('7)iltimos birinchi son kiriting')
+// const num11 = +prompt('iltimos ikkinchi sonni ham kiriting')
+
+// if (num10 < num11) {
+//     console.log(`birinchi son kichik ${num10} < ${num11} dan`)
+// } else {
+//     console.log(`ikkinchi son kichik ${num11} < ${num10}
+//         dan`)
+// }
+
+
+
+
+
+
+
+// const a = +prompt('9)iltimos birinchi son kiriting')
+// const b = +prompt('iltimos ikkinchi sonni ham kiriting')
+// const info = (a * 0 + a);
+// const info2 = (b * 2 + a);
+// if (info < info2) {
+//     alert(`birinchi son ${info}<${info2} dan`);
+// } else {
+//     alert("eror");
+// }
+
+
+
+
+
+
+
+// let ten = +prompt("11)ultimos son kiriting");
+// let eleven = +prompt("ultimos son kiriting");
+// if (ten > eleven) {
+//     alert(ten + eleven);
+// } else if (ten < eleven) {
+//     alert(ten + eleven);
+// } else if (ten == eleven) {
+//     alert(0);
+// }
+
+
+
+
+
+
+
+// let ten = +prompt("12)ultimos son kiriting");
+// let eleven = +prompt("ultimos son kiriting");
+// if (ten > eleven) {
+//     alert(ten);
+// } else if (eleven > ten) {
+//     alert(eleven);
+// } else if (ten == eleven) {
+//     alert(0);
+// }
+
+
+
+
+
+// let number1 = prompt('iltimos birinchi sonni kiriting');
+// let number2 = prompt('iltimos birinchi sonni kiriting');
+// let number3 = prompt('iltimos birinchi sonni kiriting');
+// if (number1 < number2 && number2 > number3) {
+//     alert(number2);
+// } else if (number1 < number3 && number2 < number1) {
+//     alert(number1);
+// } else {
+//     alert(number3);
+// }
+
+
+
+
+
+
+// let number = +prompt('Iltimos raqam kiriting');
+// switch (number) {
+//     case 1:
+//         alert('dushanba');
+//         break;
+//     case 2:
+//         alert('seshanba');
+//         break;
+//     case 3:
+//         alert('chorshanba');
+//         break;
+//     case 4:
+//         alert('payshanba');
+//         break;
+//     case 5:
+//         alert('juma');
+//         break;
+//     case 6:
+//         alert('shaba');
+//         break;
+//     case 7:
+//         alert('yakshanba');
+//         break;
+//     default:
+//         alert("bunaqa kun toq")
+//         break;
+// }
+
+
+
+
+
+// function number(name) {
+//     if (name % 2 === 0) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+
+// console.log(number(14));
+// console.log(number(15));
+
+
+
+
+
+
+
+// let nan = +prompt("iltimos son kiriting")
+
+// function name(a) {
+//     return a ** 3
+// }
+// alert(name(nan))
+
+
+
+
+
+// function name(a, b, c) {
+//     return `son bir ${Math.sqrt(a)} son ikki ${Math.sqrt(b)} son uch ${Math.sqrt(c)}`
+// }
+// alert(name(80, 64, 25))
+
+
+
+
+
+
+
+// let myArayy = [1998, 2001, 2000, 2003, 2009];
+
+// const funi = function(params) {
+//     if (params >= 1999 && 2000 <= params) {
+//         return (`Siz 21 asrda tugulgansiz | sizning yoshingiz ${2023-myArayy} da`);
+//     } else {
+//         return ("Siz 20 asrda tugulgansiz");
+//     }
+// }
+// for (let index = 0; index < myArayy.length; index++) {
+//     console.log(funi(myArayy[index]));
+
+// }
+
+
+
+
+
+
+
+
+
+// const input = document.querySelector(".input");
+// const btn = document.querySelector(".btn");
+// const box = document.querySelector(".box");
+
+// btn.addEventListener("click", function() {
+//     const reon = input.value.split("").reverse().join("");
+
+//     box.textContent = reon
+// })
+
+
+
+
+
+
+
+// const numbers = +prompt("Raqam kiriting")
+// const num = []
+// const addnum = []
+// const oddnum = []
+
+
+// for (let index = 0; index < numbers; index++) {
+//     if (index % 2 == 0) {
+//         addnum.push(index)
+//     } else {
+//         oddnum.push(index)
+//     }
+
+// }
+// console.log(addnum);
+// console.log(oddnum);
+
+
+
+
+
+
+
+
+// const num2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const total = 0;
+// for (let index = 0; index < num2.length; index++) {
+//     total2 = total + num2
+//     console.log(total2[index]);
+// }
+
+
+
+
+
+// const numbers = [10, 54, 55, 83, 12, 4, 9, 70, 5, 39];
+// let minnum = [];
+// let maxnum = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     minnum = Math.max(...numbers);
+//     maxnum = Math.min(...numbers);
+// }
+// console.log(minnum, maxnum);
+
+
+const form = document.querySelector(".form");
+const input = document.querySelector(".input");
+const box = document.querySelector(".box");
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const resolt = document.createElement("div");
+
+    resolt.classList.add("box_menu");
+    let span = document.createElement("span");
+    const img = document.createElement('img');
+    img.classList.add("image_delite")
+    img.src = 'https://avatars.mds.yandex.net/i?id=7f406633d232c7e5aed8dff7f91b6243c66c1ac6-7755287-images-thumbs&n=13'
+    span.textContent = input.value
+    img.textContent = input.value
+
+    const btn = document.createElement('button');
+    btn.classList.add("botton")
+    resolt.appendChild(span)
+    btn.appendChild(img);
+
+
+    resolt.appendChild(btn);
+
+    box.appendChild(resolt);
+
+    btn.addEventListener("click", () => {
+        box.removeChild(resolt)
+    })
+})
+
+
+
+
+
+
+
+// const form = document.querySelector('.form'),
+//     name = document.querySelector('.name'),
+//     relation = document.querySelector('.relation'),
+//     number = document.querySelector('.number'),
+//     btn4 = document.querySelector('.btn'),
+//     result = document.querySelectorAll('.resuld');
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+
+//     const result = document.querySelector('.resuld');
+//     result.classList.add('contact');
+
+//     let nameSpan = document.createElement('span')
+//     nameSpan.textContent = name.value;
+//     result.appendChild(nameSpan);
+
+//     let relatSpan = document.createElement('span')
+//     relatSpan.textContent = relation.value;
+//     result.appendChild(relatSpan);
+
+//     let numberSpan = document.createElement('span')
+//     numberSpan.textContent = number.value;
+//     result.appendChild(numberSpan);
+
+//     let removeBtn = document.createElement('button')
+//     removeBtn.textContent = 'delete';
+//     result.appendChild(removeBtn);
+
+//     removeBtn.addEventListener('click', () => {
+//         result.removeChild(nameSpan)
+//     })
+//     removeBtn.addEventListener('click', () => {
+//         result.removeChild(relatSpan)
+//     })
+//     removeBtn.addEventListener('click', () => {
+//         result.removeChild(numberSpan)
+//     })
+//     removeBtn.addEventListener('click', () => {
+//         result.removeChild(removeBtn)
+//     })
+
+//     form.reset();
+// })
+
+// btn.addEventListener("click", () => {
+
+
+// })
+
+
+
+
+
+// const n = 6;
+// let resolt = 1;
+// for (let i = 1; i < n; i++) {
+
+//     let rest = resolt += i
+//         // for (let t = 0; rest < rest; t++) {
+
+
+//     // }
+// }
